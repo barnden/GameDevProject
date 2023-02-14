10e6410505b16a3693a16472dd98f13087326420
Jason Hirsch <pythonpotatostuff@gmail.com>
2023-02-14 12:29:59 -0600

    Squashed commit of the following:
    commit fa3ce0ca175d37f9eb74b7d273fb9dcdcf549346
    Author: Jason Hirsch <pythonpotatostuff@gmail.com>
    Date:   Tue Feb 14 12:29:42 2023 -0600
    
        Added WASD movement
    
    commit 00d487dd343a83ebced6db1e27bf11902be77d10
    Author: Jason Hirsch <pythonpotatostuff@gmail.com>
    Date:   Mon Feb 13 15:24:14 2023 -0600
    
        Scene changes
    
    commit c70645fd9c538cf27d15bd471ea6a18b2e89651b
    Author: Jason Hirsch <pythonpotatostuff@gmail.com>
    Date:   Mon Feb 13 15:23:58 2023 -0600
    
        Camera zoom and centering

---
+++ Assets/Camera/CameraMovement.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private Camera cameraObject;
+    [SerializeField] private GameObject baseObject;
+
+    [SerializeField] private bool baseCentered = true;
+    [SerializeField] private bool scrollBase = true;
+    [SerializeField] private float baseCenteredSize;
+    [SerializeField] private float baseCenteringSpeed;
+
+    [SerializeField] private float cameraMaxSize;
+    [SerializeField] private float cameraMinSize;
+    [SerializeField] private float cameraSpeed;
+
+    void Update()
+    {
+        
+
+        if(Input.mouseScrollDelta.y != 0)
+        {
+            scrollBase = false;
+            cameraObject.orthographicSize = Mathf.Max(Mathf.Min(cameraObject.orthographicSize - Input.mouseScrollDelta.y, cameraMaxSize), cameraMinSize);
+        }
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        gameObject.transform.position += new Vector3(horizontal * cameraSpeed, vertical * cameraSpeed, 0.0f);
+
+        if(horizontal != 0.0f || vertical != 0.0f)
+        {
+            baseCentered = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            baseCentered = true;
+            scrollBase = true;
+        }
+        
+        if (baseCentered)
+        {
+            Vector3 pos = gameObject.transform.position;
+            Vector3 basePos = baseObject.transform.position;
+
+            Vector3 cameraDelta = new Vector3((basePos.x - pos.x) / baseCenteringSpeed, (basePos.y - pos.y) / baseCenteringSpeed, 0.0f);
+            gameObject.transform.position += cameraDelta;
+
+            if(gameObject.transform.position.x == basePos.x && gameObject.transform.position.y == basePos.y)
+            {
+                baseCentered = false;
+            }
+        }
+
+        if(scrollBase)
+        {
+            cameraObject.orthographicSize += (baseCenteredSize - cameraObject.orthographicSize) / baseCenteringSpeed;
+            if (baseCenteredSize == cameraObject.orthographicSize)
+            {
+                scrollBase = false;
+            }
+        }
+    }
+}
