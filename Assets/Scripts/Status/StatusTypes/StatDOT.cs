a7144e0748801eb2e33179a60789a8c8aac0c38f
SeanJKelly <seanjosephkelly14@hotmail.com>
2023-03-07 20:21:55 -0600

    Added DOT status effect
    - Damage of time status effect implemented
    - Example provided

---
+++ Assets/Scripts/Status/StatusTypes/StatDOT.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* As of right now, DOTs are on a per-second basis
+ * so every second the damage/healing will be applied
+ * to keep computation power to a minimum
+ */
+
+[CreateAssetMenu(fileName = "DOT", menuName = "StatusTypes/StatDOT", order = 1)]
+public class StatDOT : BaseStatusEffect
+{
+    public int Duration;
+    public override void Apply(StatusSystem entityStatSysToEffect,
+                                BaseAIComponent compToEffect)
+    {
+        IEnumerator coroutine = DoEffect(entityStatSysToEffect, compToEffect);
+        entityStatSysToEffect.StartCoroutine(coroutine);
+    }
+    private IEnumerator DoEffect(StatusSystem entityStatSysToEffect, BaseAIComponent compToEffect)
+    {
+        int counter = this.Duration;
+        while (counter > 0)
+        {
+            compToEffect.DamageStat(statToEffect, amount);
+            counter--;
+            yield return new WaitForSeconds(1.0f);
+        }
+
+        entityStatSysToEffect.RemoveEffect(this);
+    }
+}
--- Assets/Scripts/Status/StatusTypes/StatDamageInstant.cs
+++ Assets/Scripts/Status/StatusTypes/StatDamageInstant.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Damage", menuName = "StatusTypes/StatDamageInstant", order = 1)]
+[CreateAssetMenu(fileName = "Damage Instant", menuName = "StatusTypes/StatDamageInstant", order = 1)]
 public class StatDamageInstant : BaseStatusEffect
 {
     public override void Apply(StatusSystem entityStatSysToEffect,
